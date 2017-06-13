1d30447754104e78580eec176c7c3e07d6a29363
MAJESTIC123 <karthiece996@gmail.com>
2017-06-13 13:56:33 +0530

    Add files via upload

---
+++ flow2.java
@@ -0,0 +1,21 @@
+package sk;
+import java.util.Scanner;
+public class flow2 {
+public static void main(String[]args){
+int n;
+Scanner a=new Scanner(System.in);
+System.out.println("enter the given number");
+n=a.nextInt();
+if(n%2==0)
+{
+System.out.println("the given number"+n+" is even");
+}
+else{
+System.out.println("the given number "+n+" is odd");
+}
+}
+}
+
+
+
+
